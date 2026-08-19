47d82a288ad7d5344aaad01e31eb0bafa1b1cbab
tomfarr90-java <tomfarr90@gmail.com>
2026-08-19 16:08:34 +0300

    Внесены изменения в классы

--- src/main/java/ru/javarush/dao/CountryDAO.java
+++ src/main/java/ru/javarush/dao/CountryDAO.java
@@ -1,4 +1,21 @@
 package ru.javarush.dao;
 
+
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import ru.javarush.domain.Country;
+
+import java.util.List;
+
 public class CountryDAO {
+    private final SessionFactory sessionFactory;
+
+    public CountryDAO(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    public List<Country> getAll() {
+        Query<Country> query = sessionFactory.getCurrentSession().createQuery("select c from Country c join fetch c.languages", Country.class);
+    return query.list();
+    }
 }
--- src/main/java/ru/javarush/domain/Continent.java
+++ src/main/java/ru/javarush/domain/Continent.java
@@ -1,4 +1,11 @@
 package ru.javarush.domain;
 
 public enum Continent {
+    Asia,
+    Europe,
+    North_America,
+    Africa,
+    Oceania,
+    Antarctica,
+    South_America
 }
